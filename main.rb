df74010d5dbf3dbfda0c53bf827c409333887729
KristenOrue <oruek@oregonstate.edu>
2021-05-07 17:34:42 -0700

    Got too far down the rabbit hole undisciplined so starting over

--- main.rb
+++ main.rb
@@ -1,20 +1,30 @@
 #Kristen Orue 
 #Exercise 6
 
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+def draw_button(label_text, x, y)
+  paint(x, y)
+end
+
+def paint(label_text, x, y, is_dark_mode)
   if is_dark_mode
-    # darken foreground color for dark mode
     darken_foreground()
   else
-    # lighten foreground color for non-dark mode
     lighten_foreground()
   end
 end
 
-def darken_foreground
-  paint(label_text, x, y, foreground_color - 10, '#111111')
+def background_color(is_dark_mode)
+  if is_dark_mode
+    background_color = '#111111'
+  else
+    background_color = '#E0E0E0'
+  end
 end
 
-def lighten_foreground
-  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+def darken_foreground(foreground_color)
+  foreground_color - 10
+end
+
+def lighten_foreground(foreground_color)
+  foreground_color + 10
 end
