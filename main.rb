abbab319a6f0e1ec010afd0650c8f337321b24fd
KristenOrue <oruek@oregonstate.edu>
2021-05-07 17:41:49 -0700

    Extract too many functions

--- main.rb
+++ main.rb
@@ -2,18 +2,16 @@
 #Exercise 6
 
 def draw_button(label_text, x, y)
-  paint(x, y)
+  paint()
 end
 
-def paint(label_text, x, y, is_dark_mode)
-  if is_dark_mode
-    darken_foreground()
-  else
-    lighten_foreground()
-  end
+def paint()
+  set_background_color()
+  set_foreground_color()
 end
 
-def background_color(is_dark_mode)
+
+def set_background_color(is_dark_mode)
   if is_dark_mode
     background_color = '#111111'
   else
@@ -21,10 +19,18 @@ def background_color(is_dark_mode)
   end
 end
 
-def darken_foreground(foreground_color)
-  foreground_color - 10
+def set_foreground_color(is_dark_mode)
+  if is_dark_mode
+    foreground_color - 10
+  else
+    foreground_color + 10
+  end
 end
 
-def lighten_foreground(foreground_color)
-  foreground_color + 10
-end
+# def darken_foreground(foreground_color)
+#   foreground_color - 10
+# end
+
+# def lighten_foreground(foreground_color)
+#   foreground_color + 10
+# end
