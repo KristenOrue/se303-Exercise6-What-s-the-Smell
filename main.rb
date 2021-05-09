d422fe5267a752319b770219c4f7cb3258540d26
KristenOrue <oruek@oregonstate.edu>
2021-05-09 12:22:21 -0700

    Extract Functions from code comments

--- main.rb
+++ main.rb
@@ -3,12 +3,18 @@
 
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   if is_dark_mode
-    # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    darken_foreground
   else
-    # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    lighten_foreground
   end
+end
+
+def darken_foreground(label_text, x, y, foreground_color, is_dark_mode)
+  paint(label_text, x, y, foreground_color - 10, '#111111')
+end
+
+def lighten_foreground(label_text, x, y, foreground_color, is_dark_mode)
+  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
 end
 
 class Button
